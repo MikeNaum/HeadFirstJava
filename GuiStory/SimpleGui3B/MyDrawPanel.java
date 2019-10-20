aa9d311c104124fffdaeb27a298c0f0e3104829b
MikeNaum <mihailnaumenko@gmail.com>
2019-10-20 12:14:17 +0300

    add to MyDrawPanel функцию заливки фона

--- GuiStory/SimpleGui3B/MyDrawPanel.java
+++ GuiStory/SimpleGui3B/MyDrawPanel.java
@@ -6,6 +6,8 @@ class MyDrawPanel extends JPanel {
 	public void paintComponent(Graphics g) {
 		Graphics2D g2d = (Graphics2D) g;
 		
+		g.fillRect(0, 0, this.getWidth(), this.getHeight());
+		
 		int red = (int) (Math.random() * 255);
 		int green = (int) (Math.random() * 255);
 		int blue = (int) (Math.random() * 255);
